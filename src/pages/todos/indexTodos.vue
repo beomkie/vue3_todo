673c571752ae2e28f2c6e19c761888baef065205
Lee Gibeom <gb3205@naver.com>
2024-02-03 13:49:12 +0900

    mini

---
+++ src/pages/todos/indexTodos.vue
@@ -0,0 +1,168 @@
+<template>
+  <router-view></router-view>
+  <div class="container">
+    <h2>To Do List</h2>
+    <input
+      class="form-control"
+      type="text" 
+      v-model="searchText"
+      search="Type New To do"
+      placeholder="Search"
+      >
+      <hr />
+    <TodoSimpleForm @add-todo="addTodo"/>
+    <div style="color:red;">{{ error }}</div>
+    
+    <div v-if="!filteredTodos.length">
+      There is nothing to display
+    </div>
+    <TodoList
+    :todos="filteredTodos" 
+    @toggle-todo="toggleTodo" 
+    @delete-todo="deleteTodo"
+    />
+    <hr />
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li v-if="currentpage !== 1" class="page-item"><a class="page-link" @click="getTodos(currentpage-1)" href="#">Previous</a></li>
+
+        <li 
+          class="page-item" 
+          :class="currentpage == page ? 'active' : ''"
+          v-for="page in 3" 
+          :key="page"
+        >
+          <a class="page-link" href="#">{{ page }}</a>
+        </li>
+        
+        <li v-if=" numberOfPages !== currentpage" class="page-item"><a class="page-link" @click="getTodos(currentpage+1)" href="#">Next</a></li>
+      </ul>
+    </nav>
+  </div>  
+</template>
+
+<script>
+import { ref, computed, watchEffect } from 'vue';
+import TodoSimpleForm from '@/components/TodoSimpleForm.vue';
+import TodoList from '@/components/TodoList.vue';
+import axios from 'axios';
+
+export default {
+  components: {
+    TodoSimpleForm,
+    TodoList,
+  },
+  setup() {
+    const todos = ref([]);
+    const todoStyle = {
+      textDecoration: 'line-through',
+      color: 'gray'
+    }
+    const error = ref('');
+    const numberOfTodos = ref(0);
+    const limit = 5;
+    const currentpage = ref(1);
+
+    watchEffect(() => {
+      console.log(currentpage.value);
+      console.log(numberOfTodos.value);
+    })
+        
+    const numberOfPages = computed(() => {
+      return Math.ceil(numberOfTodos.value/limit);
+    });
+
+    // x-total-count가 헤더에 존재하지 않는 것 같음. 작동하지 않음
+    const  getTodos = async () => {
+      try {
+        const res = await axios.get(`http://localhost:3000/todos?_page=${currentpage.value}&_limit=${limit}`);
+        numberOfTodos.value = res.headers['x-total-count'];
+        todos.value = res.data;
+        console.log('All Headers:', res.headers);
+
+      } catch(err) {
+        error.value = 'something went wrong.';
+      }
+    }
+
+    getTodos();
+    console.log(numberOfTodos.value);
+    
+    const addTodo = async (todo) => {
+      //데이터베이스에 투두를 저장하기
+      error.value='';
+      try {
+        const res = await axios.post('http://localhost:3000/todos', {
+          subject: todo.subject,
+          completed: todo.completed,
+        });
+        todos.value.push(res.data);
+      } catch (err) {
+        error.value = 'something went wrong.';
+      }
+    };
+
+    const toggleTodo = async (index) => {
+      error.value = '';
+      const id = todos.value[index].id;
+      try{
+        await axios.patch('http://localhost:3000/todos/' + id, {
+          completed: !todos.value[index].completed
+        });
+        todos.value[index].completed = !todos.value[index].completed
+      } catch(err) {
+        console.log(err)
+        error.value = 'something went wrong.';
+      }
+      }
+  
+
+    const deleteTodo = async (index) => {
+      error.value = '';
+      const id = todos.value[index].id;
+      try{
+        await axios.delete('http://localhost:3000/todos/' + id);
+        todos.value.splice(index, 1);    
+
+      } catch (err) {
+        console.log(err);
+        error.value = 'something went wrong.';
+      }
+    };
+
+    const searchText = ref('');
+    const filteredTodos = computed(() => {
+      if(searchText.value) {
+        return todos.value.filter(todo => {
+          return todo.subject.includes(searchText.value);
+        });
+      }
+      return todos.value;
+    })
+  
+
+    return {
+      addTodo,
+      todos,
+      todoStyle,
+      deleteTodo,
+      toggleTodo,
+      searchText,
+      filteredTodos,
+      error,
+      numberOfPages,
+      currentpage,
+    }
+
+  },
+
+    
+}
+</script>
+
+<style>
+  .todo {
+    color: gray;
+    text-decoration: line-through;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../pages/indexHome.vue'
+import Todos from '../pages/todos/indexTodos.vue'
 
 const router = createRouter({
     history: createWebHistory(),
-    routes: []
+    routes: [
+        {
+            path: '/',
+            name: 'Home',
+            component: Home
+        },
+        {
+            path: '/todos',
+            name: 'Todos',
+            component: Todos
+        }
+    ]
 })
 
 export default router;
